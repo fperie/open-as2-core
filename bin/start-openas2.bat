0865a8a27beb63be41b1dd3a3cd92812308787ca
Fabien PERIE <fabien.perie@gmail.com>
2015-05-22 20:13:15 +0200

    clone open as2 project.

---
+++ bin/start-openas2.bat
@@ -0,0 +1,7 @@
+#!/bin/sh
+# purpose: runs the OpenAS2 application     
+set JAVA_EXE=%java_home%\bin\java 
+#    
+# remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package    
+#
+"%JAVA_EXE%" -Xms32m -Xmx384m -Dorg.apache.commons.logging.Log=org.openas2.logging.Log  -cp .;../lib/activation.jar;../lib/mail.jar;../lib/bcprov-jdk14-125.jar;../lib/bcmail-jdk14-125.jar;../lib/commons-logging-1.1.1.jar;../lib/OpenAS2_20100816.jar org.openas2.app.OpenAS2Server ../config/config.xml
