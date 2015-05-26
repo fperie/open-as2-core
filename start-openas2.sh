1118ad0096c36760f3c72b72cdeb2d2c1520105b
Fabien PERIE <perie.f@mipih.fr>
2015-05-26 11:54:16 +0200

    remove start-openas2.sh file, you must use the eclipse launcher.

--- start-openas2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export JAVA_EXE=$JAVA_HOME\\bin\\java 
-
-# Purpose:  runs the OpenAS2 application
-#    
-# rem remove -Dorg.apache.commons.logging.Log=org.openas2.logging.Log if using another logging package
-# rem
-$JAVA_EXE -Xms32m -Xmx384m -Dorg.apache.commons.logging.Log=org.openas2.logging.Log  -cp .:../lib/activation.jar:../lib/mail.jar:../lib/bcprov-jdk14-125.jar:../lib/bcmail-jdk14-125.jar:../lib/commons-logging-1.1.1.jar:../lib/OpenAS2_20100816.jar org.openas2.app.OpenAS2Server ../config/config.xml
